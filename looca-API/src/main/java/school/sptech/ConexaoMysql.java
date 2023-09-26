bac335beba5b877cbebb09fcdebdbb739be477b5
KauanCursino <kauan.souza@sptech.school>
2023-09-26 12:58:05 -0300

    feat(Looca): conexao com o banco de dados

---
+++ looca-API/src/main/java/school/sptech/ConexaoMysql.java
@@ -0,0 +1,26 @@
+package school.sptech;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class ConexaoMysql {
+    private JdbcTemplate conexaoBanco;
+
+    public ConexaoMysql(){
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost/bankSecure");
+
+        /*MUDAR PARA O SEU USUÁRIO MYSQL PARA TESTES*/
+        dataSource.setUsername("");
+        dataSource.setPassword("");
+
+        conexaoBanco = new JdbcTemplate(dataSource);
+
+
+
+    }
+    public JdbcTemplate getConexaoBanco() {
+        return conexaoBanco;
+    }
+}
