af8ad993e98fc0c81d732ca309777f7d2a6d1fbf
anabueno25 <125586664+anabueno25@users.noreply.github.com>
2023-11-28 17:37:52 -0300

    feat(projeto-slack): inserindo slack com java

---
+++ projeto-slack/src/main/java/school/sptech/Main.java
@@ -0,0 +1,13 @@
+package school.sptech;
+
+public class Main {
+    public static void main(String[] args) {
+        ConexaoMysql conexaoMysql = new ConexaoMysql();
+
+
+        conexaoMysql.consultarETEnviarParaSlack();
+
+        
+
+    }
+}
